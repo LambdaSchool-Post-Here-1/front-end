953a1fe62f571c903205fa81b9e6d2ac3f02698b
seanmx96 <seanmx96@users.noreply.github.com>
2020-05-26 13:29:19 -0500

    Removed Switch import

--- post-here/src/App.js
+++ post-here/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link, Switch } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import PrivateRoute from './components/PrivateRoute';
 

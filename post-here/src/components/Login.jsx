9c3cd3582547919f6d12cd8488a7c083e13b1cab
martinezmarina <martinezmarina980@gmail.com>
2020-05-26 20:05:44 -0400

    login now posts info to API and console logs from API

--- post-here/src/components/Login.jsx
+++ post-here/src/components/Login.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const initialFormValues = {
   username: "",
@@ -6,41 +7,48 @@ const initialFormValues = {
 };
 
 function Login(props) {
-
-  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formValues, setFormValues] = useState(initialFormValues);
 
   const onInputChange = (event) => {
     const { name } = event.target;
     const { value } = event.target;
     setFormValues({ ...formValues, [name]: value });
-  }
+  };
   const onSubmit = (event) => {
     event.preventDefault();
     //what do with login information object?
-    console.log(formValues)
-    setFormValues(initialFormValues)
-  }
+    axios
+      .post("https://post-here-heroku.herokuapp.com/api/auth/login", formValues)
+      .then((res) => {
+        console.log(res.data)
+      })
+      .catch((err) => {
+        console.log("error")
+      });
+    console.log(formValues);
+    setFormValues(initialFormValues);
+  };
 
   return (
     <div className="login">
       <form className="formContainer" onSubmit={onSubmit}>
         <h2>Login</h2>
         <label>
-          <input 
-          type="text" 
-          name="username" 
-          placeholder="UserName"
-          value={formValues.username}
-          onChange={onInputChange}
+          <input
+            type="text"
+            name="username"
+            placeholder="UserName"
+            value={formValues.username}
+            onChange={onInputChange}
           />
         </label>
         <label>
-          <input 
-          type="password" 
-          name="password" 
-          placeholder="Password" 
-          value={formValues.password}
-          onChange={onInputChange}
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formValues.password}
+            onChange={onInputChange}
           />
         </label>
 
